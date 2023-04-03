ca83522136522c0e0f2b8bff28698b3808860d95
mnky729 <mnky729@gmail.com>
2023-04-04 03:32:20 +0700

    add the runtime fn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::collections::HashSet;
 use crate::sudoku_starter::*;
+use std::time::{Duration, Instant};
 
 mod sudoku_starter;
 
-fn main() {
-    println!("Hello, world!");
-    let board = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
+fn time_solve(board: &str) -> (HashSet<Vec<Vec<u32>>>, Duration) {
+    let starting_point = Instant::now();
     let solutions = solve(board);
-    println!("Total solutions found: {}", solutions.len());
+    let elapsed_time = starting_point.elapsed();
+    (solutions, elapsed_time)
+}
+
+fn main() {
+    let board = "53..7....6..195....98....6.8...6...34..8.3..17...2...6.6....28....419..5....8..79";
+    let (solutions, elapsed_time) = time_solve(board);
     println!("Solutions:");
-    show_solutions(solutions);
+    show_solutions(solutions.clone());
+    println!("Total solutions found: {}", solutions.len());
+    println!("Elapsed time: {:?}", elapsed_time);
 }
